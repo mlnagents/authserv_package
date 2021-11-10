afe45c2500dd002728be4ae76dbdaf07c2453ccd
v.egorov <slava.eg@yandex.ru>
2021-11-10 22:49:04 +0300

    fix

--- authserv/clients/auth.py
+++ authserv/clients/auth.py
@@ -7,11 +7,13 @@ class AuthServ:
     api_path = "api/v1"
     headers = {"X-Token": settings.AUTHSERV_TOKEN, "Content-Type": "application/json"}
 
-    def auth(self, username, password):
+    def auth(self, username, password, user_agent=None, ip=None):
         resp = requests.post(
             url=f"{settings.AUTHSERV_HOST}/{self.api_path}/auth", data=json.dumps({
                 "username": username,
-                "password": password
+                "password": password,
+                "user_agent": user_agent,
+                "ip": ip
             }), headers=self.headers
         )
         return {"status": resp.status_code, "body": resp.json()}
